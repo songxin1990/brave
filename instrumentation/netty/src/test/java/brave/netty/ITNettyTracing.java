ba1b3f6efe44d0ba0b2b284d4539214bb7508522
e <xin.song@cimc.com>
2017-07-01 17:10:38 +0800

    test 4

--- instrumentation/netty/src/test/java/brave/netty/ITNettyTracing.java
+++ instrumentation/netty/src/test/java/brave/netty/ITNettyTracing.java
@@ -2,17 +2,20 @@ package brave.netty;
 
 import brave.http.ITHttpServer;
 import org.junit.After;
+import org.junit.AssumptionViolatedException;
+import org.junit.ComparisonFailure;
+import org.junit.Test;
 
 public class ITNettyTracing extends ITHttpServer {
-  private int port=4567;
+  private int port = 4567;
   HttpSnoopyServer httpSnoopServer = null;
   boolean inited = false;
 
-/*  @Override
+  @Override
   @Test(expected = ComparisonFailure.class)
   public void reportsClientAddress() throws Exception {
     throw new AssumptionViolatedException("client address can get from channel not request");
-  }*/
+  }
 
   @Override
   protected void init() throws Exception {
